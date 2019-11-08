4e82348bc8f02f1208ee6fd44bd1e92a1db98529
AstusRush <a011235robin@gmail.com>
2019-11-08 16:11:41 +0100

    Windows batch fix
    The install .bat now installs amadia to:
    %USERNAME%/AMaDiA/
    instead of
    %USERNAME%/AMaDiA/AMaDiA/
    
    AMaDiA.bat now checks two locations for for the Anaconda installation:
    The %USERNAME%folder and (if that fails) ProgramData

--- WINDOWS-INSTALL/AMaDiA.bat
+++ WINDOWS-INSTALL/AMaDiA.bat
@@ -1,5 +1,11 @@
 cd "C:\Users\%USERNAME%\AMaDiA"
 git pull
-call "C:\Users\%USERNAME%\Anaconda3\Scripts\activate.bat"
-"C:\Users\%USERNAME%\Anaconda3\python.exe" "C:\Users\%USERNAME%\AMaDiA\AMaDiA.py"
+%@Try%
+  call "C:\Users\%USERNAME%\Anaconda3\Scripts\activate.bat"
+  "C:\Users\%USERNAME%\Anaconda3\python.exe" "C:\Users\%USERNAME%\AMaDiA\AMaDiA.py"
+%@EndTry%
+:@Catch
+  call "C:\ProgramData\Anaconda3\Scripts\activate.bat"
+  "C:\ProgramData\Anaconda3\python.exe" "C:\Users\%USERNAME%\AMaDiA\AMaDiA.py"
+:@EndCatch
 if NOT ["%errorlevel%"]==["0"] pause
--- WINDOWS-INSTALL/AMaDiA_INSTALL.bat
+++ WINDOWS-INSTALL/AMaDiA_INSTALL.bat
@@ -1,4 +1,2 @@
 cd "C:\Users\%USERNAME%"
-mkdir AMaDiA
-cd "C:\Users\%USERNAME%\AMaDiA"
 git clone https://github.com/AstusRush/AMaDiA
