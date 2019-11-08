6228db3cd99279e7a9a5f476f2ece38312f29714
AstusRush <a011235robin@gmail.com>
2019-11-08 11:45:59 +0100

    Update AMaDiA.bat
    Fixed a bug

--- WINDOWS-INSTALL/AMaDiA.bat
+++ WINDOWS-INSTALL/AMaDiA.bat
@@ -1,5 +1,5 @@
 cd "C:\Users\%USERNAME%\AMaDiA"
-git pull https://github.com/AstusRush/AMaDiA
+git pull
 call "C:\Users\%USERNAME%\Anaconda3\Scripts\activate.bat"
 "C:\Users\%USERNAME%\Anaconda3\python.exe" "C:\Users\%USERNAME%\AMaDiA\AMaDiA.py"
 if NOT ["%errorlevel%"]==["0"] pause
